d45c2b1da28a2ee3c65c9ed926b03372c2095730
SeifMohy <seifmohy@gmail.com>
2022-04-20 14:16:21 +0200

    socket

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,46 @@
-import cors from 'cors'
-import dotenv from 'dotenv'
-import express, { json, urlencoded } from 'express'
-import helmet from 'helmet'
-import morgan from 'morgan'
-import AppDataSource from './data-source'
-import { authRouter } from './routes/auth'
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { json, urlencoded } from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import AppDataSource from "./data-source";
+import { authRouter } from "./routes/auth";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
-const app = express()
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:7090",
+  },
+});
 
-dotenv.config()
+dotenv.config();
 
-app.use(cors())
-app.use(morgan('dev'))
-app.use(helmet())
-app.use(json())
-app.use(urlencoded({ extended: false }))
+app.use(cors());
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(json());
+app.use(urlencoded({ extended: false }));
 
-app.use('/auth', authRouter)
+app.use("/auth", authRouter);
 
-app.get('/', function (req, res) {
-  res.send('server is running')
-})
-app.get('/chat', function (req, res) {
-    res.sendFile(__dirname + '/index.html')
-  })
+app.get("/", function (req, res) {
+  res.send("server is running");
+});
 
-  io.on('connection', (socket) => {
-    console.log('a user connected');
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
   });
-
-app.listen(process.env.PORT || 4545, async () => {
-  await AppDataSource.initialize()
-  console.log('connected to DB')
-})
+  socket.on("message", (message)=>{
+      console.log(message);
+      io.emit("new message", message);
+  })
+});
+server.listen(process.env.PORT || 4545, async () => {
+  await AppDataSource.initialize();
+  console.log("connected to DB");
+});
